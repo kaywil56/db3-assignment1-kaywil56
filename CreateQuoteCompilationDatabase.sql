ec59496a7bc86d70dc5dfaa00eabe756d5799fe0
kaylem williams <kaywil56>
2023-05-05 09:56:48 +1200

    add more create tables

--- CreateQuoteCompilationDatabase.sql
+++ CreateQuoteCompilationDatabase.sql
@@ -1,14 +1,19 @@
-drop schema if exists quote_compilation
-create schema quote_compilation
-use quote_compilation
+drop table if exists Supplier
+drop table if exists Category
+drop table if exists Component
+drop table if exists Contact
+drop table if exists Customer
+drop table if exists Quote
+drop table if exists QuoteComponent
+
+create table Supplier(
+	SupplierID int primary key not null,
+	SupplierGST decimal not null
+)
 
 create table Category(
 	CategoryID int primary key not null,
 	CategoryName nvarchar(100) not null
-)
-
-create table Supplier(
-	SupplierID
 )
 
 create table Component(
@@ -17,6 +22,46 @@ create table Component(
 	ComponentDescription nvarchar(100) not null,
 	TradePrice int not null,
 	TimeToFit decimal not null, 
-	constraint fk_category foreign key (CategoryID) references Category(CategoryID)
-	constraint fk_supplier foreign key (SupplierID) references Supplier(SuplierID)
+	CategoryID int not null,
+	SupplierID int not null,
+	constraint fk_category foreign key (CategoryID) references Category(CategoryID),
+	constraint fk_supplier foreign key (SupplierID) references Supplier(SupplierID)
+)
+
+create table Contact(
+	ContactName nvarchar(100) not null,
+	ContactPhone bigint not null,
+	ContactFax bigint,
+	ContactMobilePhone bigint,
+	ContactEmail nvarchar(50),
+	ContactWWW nvarchar(100),
+	ContactPostalAddress nvarchar(100) not null
+)
+
+--create table AssemblySubcomponent(
+--	AssemblyID int primary key not null, 
+--	SubcomponentID int primary key not null,
+--	Quantity int
+--)
+
+create table Customer(
+	CustomerID int primary key not null
+)
+
+create table Quote(
+	QuoteID int primary key not null,
+	QuoteDescription nvarchar(100) not null,
+	QuoteDate date not null,
+	QuotePrice decimal not null,
+	--QuoteCompiler???,
+	constraint fk_customer foreign key (CustomerID) references Customer(CustomerID)
+)
+
+create table QuoteComponent(
+	Quantity int not null,
+	TradePrice decimal not null,
+	ListPrice decimal not null,
+	TimeToFit decimal not null,
+	constraint fk_component foreign key (ComponentID) references Component(ComponentID),
+	constraint fk_quote foreign key (QuoteID) references Quote(QuoteID)
 )
